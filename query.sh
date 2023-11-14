a30acf414b3100b704bd3cba2f4072f4fa404819
Yerong <yerong.li@outlook.com>
2023-11-13 23:22:37 -0600

    Test with 7b

--- query.sh
+++ query.sh
@@ -1,5 +1,6 @@
 # curl -X POST -H "Content-Type: application/json" -d '{"text": "What is this?", "model":"llava-v1.5-13b"}' http://localhost:10000/worker_generate_stream --output "result.txt"
+# --model-path "/scratch/yerong/.cache/pyllama/llava-v1.5-13b/" \ 
 python -m query   \
-  --model-path "/scratch/yerong/.cache/pyllama/llava-v1.5-7b/" \
+  --model-path "/scratch/yerong/.cache/pyllama/llava-v1.5-7b" \
   --image-file "https://llava-vl.github.io/static/images/view.jpg" \
   --load-4bit

aeae471b81a3bf093a556053f8f8cabc4d89be74
Yerong <yerong.li@outlook.com>
2023-11-13 22:52:22 -0600

    Print the type of the model

---
+++ query.py
@@ -0,0 +1,12 @@
+from llava.model.builder import load_pretrained_model
+from llava.mm_utils import get_model_name_from_path
+from llava.eval.run_llava import eval_model
+
+model_path = "/scratch/yerong/.cache/pyllama/llava-v1.5-13b/"
+
+tokenizer, model, image_processor, context_len = load_pretrained_model(
+    model_path=model_path,
+    model_base=None,
+    model_name=get_model_name_from_path(model_path)
+)
+print(type(model))

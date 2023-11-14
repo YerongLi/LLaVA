0d23d36e19f413ff648d82842daf0a96b0e66e55
Yerong <yerong.li@outlook.com>
2023-11-13 22:47:37 -0600

    Update

--- query.sh
+++ query.sh
@@ -1,1 +1,1 @@
-curl -X POST -H "Content-Type: application/json" -d '{"text": "What is this?", "model":"llava-v1.5-13b"}' http://localhost:10000/worker_generate_stream
+curl -X POST -H "Content-Type: application/json" -d '{"text": "What is this?", "model":"llava-v1.5-13b"}' http://localhost:10000/worker_generate_stream --output

3580aad6c7872206dc491395fd8e5d0d40238f58
Yerong <yerong.li@outlook.com>
2023-11-14 17:16:40 -0600

    Update query.py

--- query.sh
+++ query.sh
@@ -1,4 +1,4 @@
 # curl -X POST -H "Content-Type: application/json" -d '{"text": "What is this?", "model":"llava-v1.5-13b"}' http://localhost:10000/worker_generate_stream --output "result.txt"
 # --model-path "/scratch/yerong/.cache/pyllama/llava-v1.5-13b/" \ 
 python -m query   \
-  --model-path "/scratch/yerong/.cache/pyllama/llava-v1.5-7b"
+  --model-path "/scratch/yerong/.cache/pyllama/llava-v1.5-13b/"
